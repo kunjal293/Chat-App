782d16a314e0729f5a3d657313cfab0f8a98f85b
kunjal chachra <109844577+kunjal293@users.noreply.github.com>
2024-02-11 11:14:22 +0530

    Add files via upload

---
+++ frontend/src/components/Cat.js
@@ -0,0 +1,8 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+const Cat=()=>{
+    
+        <Avatar src="https://imgs.search.brave.com/JbS0TZaKl3wd1Jm-xG2dIKoyKYr_tuHp4zaBnQsu650/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTc0/ODQ2MjUxL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtY2F0LmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz0zNlJL/Qm4xeHZramN0NF83/QnV4RlF6VXd4Yi1h/VXJhWkZzV3pGRlVJ/d3FFPQ"/>
+      
+}
+export default Cat
